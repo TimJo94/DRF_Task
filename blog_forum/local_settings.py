6a509ca2733fd4fedd22615be55c522b90cfb1b7
TimJo94 <timurjorobekov@gmail.com>
2022-04-04 17:28:53 +0600

    first commit

---
+++ blog_forum/local_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_forum',
+        'USER': 'TimJo',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
+}
+
+SECRET_KEY = 'django-insecure-f)64v)xqk+x%a^*(u+3%$*$kwrb6277iwg%d5ctjkw5=m52@w('
